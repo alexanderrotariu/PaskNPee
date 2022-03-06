44acbb2313954275b996c697353797933b4df031
Paskul <pascalsikorskips@gmail.com>
2022-03-06 14:27:31 -0600

    Ben MOCK1

--- botMain.py
+++ botMain.py
@@ -1,5 +1,12 @@
-import discord, requests, os
+import discord, requests, os, random
 from dotenv import load_dotenv
+
+#MATH (RANDOM)
+randomInt = random.randint(0,3)
+benRunning = False
+
+##BEN RESPONSES
+benResponses = ['Yes.', 'No.', '*Slams Phone*', 'Eww']
 
 #CREDENTIALS 
 load_dotenv('.env')
@@ -12,13 +19,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global benRunning
+    global randomInt
     if message.author == client.user:
         return
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
-    if message.content.endswith("?"):
-        await message.channel.send("detected question")
+    if message.content.startswith("!benStartup"):
+        benRunning = True
+        await message.channel.send("Ben is starting up.")
+
+    while benRunning:
+        if message.content.endswith("?"):
+            await message.channel.send(benResponses[randomInt])
+            randomInt = random.randint(0,3)
+
+        if message.content.startswith("!benShutdown"):
+            benRunning = False
+            await message.channel.send("Ben is shutting down.")
+
+        ##if not message.content.startswith("!benShutdown"):
+            ##await message.channel.send('Not a valid question, end in question marks or input \"!benShutdown\" to stop reading')
+
+        ##if not message.content.endswith("?"):
+            ##await message.channel.send('Not a valid question, end in question marks or input \"!benShutdown\" to stop reading')
+
+        else:
+            return
 
 client.run(os.getenv('TOKEN'))
