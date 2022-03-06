b96ee75b1db5ea4e146b608b79822e37ea1518da
Paskul <pascalsikorskips@gmail.com>
2022-03-05 21:26:41 -0600

    9:26 PM

--- botMain.py
+++ botMain.py
@@ -15,4 +15,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
 client.run(os.getenv('TOKEN'))
